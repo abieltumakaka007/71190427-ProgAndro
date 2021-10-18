14f82455b9e44f8b3290f42d4b16a6c6df72246f
abieltumakaka <gabriel.imanuel@ti.ukdw.ac.id>
2021-10-19 00:40:07 +0700

    Pertemuan 7
    Membuat Recycle View

---
+++ Pertemuan7_71190427 rcy/app/src/main/java/id/ac/ukdw/pertemuan7/MainActivity.kt
@@ -0,0 +1,25 @@
+package id.ac.ukdw.pertemuan7
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity(){
+    val listContact = arrayListOf<Contact>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        listContact.add(Contact("TorontoTirta", "081236455768", R.mipmap.tirtatokyo))
+        listContact.add(Contact("PuppeySecret", "081233456778", R.mipmap.puppeysecret))
+        listContact.add(Contact("DendiLegend", "081384569430", R.mipmap.dendilegend))
+
+        val rcyContact = findViewById<RecyclerView>(R.id.rcyContact)
+        rcyContact.layoutManager = LinearLayoutManager(this)
+        val adapter = ContactAdapter(listContact, this)
+        rcyContact.adapter = adapter
+    }
+}
